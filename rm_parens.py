5ef9560198d177810e06cca5e02c8c06045fe79b
Tyler Roberts <tjroberts314@gmail.com>
2017-11-16 10:08:15 -0500

    Refactored into class

--- rm_parens.py
+++ rm_parens.py
@@ -4,40 +4,64 @@ import sys
 import re
 import six
 
-def unnecessary_parens(line):
-    if re.match('[ \t]*(if|while)[ \t]*\(.*\)[ \t]*:', line):
-        return True
-    return False
+class RemoveParens:
+    def __init__(self, line):
+        self.line = line
+        self._open_paren_index = None
+        self._close_paren_index = None
+        self._remove_parens()
+        
+    def _set_open_parens_index(self):
+        if 'if(' in self.line:
+            index = self.line.index('if(') + 2
+            self.line = self.line[:index] + ' ' + self.line[index:]
+        elif 'while(' in self.line:
+            index = self.line.index('while(') + 5
+            self.line = self.line[:index] + ' ' + self.line[index:]
+        # Set index to be placed at the paren
+        match = re.match('[ \t]*(if|while)[ \t]*\(', self.line)
+        open_paren_match = match[0] if six.PY3 else match.group(0)
+        self._open_paren_index = self.line.index(open_paren_match) + len(open_paren_match) - 1 
+        
+    def _set_close_parens_index(self):
+        for char_i in range(len(self.line)-1, -1, -1):
+            if self.line[char_i] == ')':
+                self._close_paren_index = char_i
+                break
     
-def remove_parens(line):
-    if 'if(' in line:
-        index = line.index('if(') + 2
-        line = line[:index] + ' ' + line[index:]
-    elif 'while(' in line:
-        index = line.index('while(') + 5
-        line = line[:index] + ' ' + line[index:]
-    # Set index to be placed at the paren
-    match = re.match('[ \t]*(if|while)[ \t]*\(', line)
-    open_paren_match = match[0] if six.PY3 else match.group(0)
-    open_paren_index = line.index(open_paren_match) + len(open_paren_match) - 1
-    close_paren_index = None
-    for char_i in range(len(line)-1, -1, -1):
-        if line[char_i] == ')':
-            close_paren_index = char_i
-            break
-    line = line[:open_paren_index] + line[open_paren_index+1:close_paren_index] + line[close_paren_index+1:]
-    return line
+    def _unnecessary_parens(self):
+        if self.e_outside_parens():
+            self._set_open_parens_index()
+            self._set_close_parens_index()           
+        cnt_matching_parens = 0
+        for i in range(self._open_paren_index+1, self._close_paren_index):
+            if self.line[i] == '(':
+                cnt_matching_parens += 1
+            elif self.line[i] == ')':
+                cnt_matching_parens -= 1
+                if cnt_matching_parens < 0:
+                        return False
+        if cnt_matching_parens == 0:
+            return True
+
+    def _outside_parens(self):
+        if re.match('[ \t]*(if|while)[ \t]*\(.*\)[ \t]*:', self.line):
+            return True
+        return False
+    
+    def _remove_parens(self):
+        if self._unnecessary_parens():
+            self.line = self.line[:self._open_paren_index] + self.line[self._open_paren_index+1:self._close_paren_index] + self.line[self._close_paren_index+1:]
 
 if __name__ == '__main__':
     file_lines = open(sys.argv[1], 'r').readlines()
     new_file_lines = []
+    updated_line = None
     with open(sys.argv[1], 'w') as file:
         try:
             for line in file_lines:
-                if unnecessary_parens(line):
-                    new_file_lines.append(remove_parens(line))
-                else:
-                    new_file_lines.append(line)
+                updated_line = RemoveParens(line)
+                new_file_lines.append(updated_line.line)
             file.write(''.join(new_file_lines))
         # If something goes wrong, write the file to it's original state
         except:
