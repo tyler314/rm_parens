02d717d8709a8a881dde67857a7404569c6561b0
Tyler Roberts <tjroberts314@gmail.com>
2017-11-16 10:42:31 -0500

    updated pep8

--- rm_parens.py
+++ rm_parens.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
+"""
+Takes a Python file in as an argument and removes unnecessary parenthesis use in if and
+while blocks. It could be an file type, but the file is assumed to have Python code and
+proper syntax.
+"""
 
 import sys
 import re
 import six
 
-class RemoveParens:
+
+class RemoveParens(object):
+    """
+    Takes a string in and removes the unnecessary parenthesis, set the line class variable
+    to the updated string. The line class variable will remain unchanged if no unnecessary
+    parenthesis exist.
+    """
     def __init__(self, line):
         self.line = line
         self._open_paren_index = None
         self._close_paren_index = None
         self._remove_parens()
-        
+
     def _set_open_parens_index(self):
         if 'if(' in self.line:
             index = self.line.index('if(') + 2
@@ -21,48 +32,52 @@ class RemoveParens:
         # Set index to be placed at the paren
         match = re.match('[ \t]*(if|while)[ \t]*\(', self.line)
         open_paren_match = match[0] if six.PY3 else match.group(0)
-        self._open_paren_index = self.line.index(open_paren_match) + len(open_paren_match) - 1 
-        
+        self._open_paren_index = self.line.index(
+            open_paren_match) + len(open_paren_match) - 1
+
     def _set_close_parens_index(self):
-        for char_i in range(len(self.line)-1, -1, -1):
+        for char_i in range(len(self.line) - 1, -1, -1):
             if self.line[char_i] == ')':
                 self._close_paren_index = char_i
                 break
-    
+
     def _unnecessary_parens(self):
-        if self.e_outside_parens():
+        if self._outside_parens():
             self._set_open_parens_index()
-            self._set_close_parens_index()           
-        cnt_matching_parens = 0
-        for i in range(self._open_paren_index+1, self._close_paren_index):
-            if self.line[i] == '(':
-                cnt_matching_parens += 1
-            elif self.line[i] == ')':
-                cnt_matching_parens -= 1
-                if cnt_matching_parens < 0:
+            self._set_close_parens_index()
+            cnt_matching_parens = 0
+            for i in range(self._open_paren_index + 1, self._close_paren_index):
+                if self.line[i] == '(':
+                    cnt_matching_parens += 1
+                elif self.line[i] == ')':
+                    cnt_matching_parens -= 1
+                    if cnt_matching_parens < 0:
                         return False
-        if cnt_matching_parens == 0:
-            return True
+            if cnt_matching_parens == 0:
+                return True
+        return False
 
     def _outside_parens(self):
         if re.match('[ \t]*(if|while)[ \t]*\(.*\)[ \t]*:', self.line):
             return True
         return False
-    
+
     def _remove_parens(self):
         if self._unnecessary_parens():
-            self.line = self.line[:self._open_paren_index] + self.line[self._open_paren_index+1:self._close_paren_index] + self.line[self._close_paren_index+1:]
+            self.line = self.line[:self._open_paren_index] + self.line[self._open_paren_index + \
+                1:self._close_paren_index] + self.line[self._close_paren_index + 1:]
+
 
 if __name__ == '__main__':
-    file_lines = open(sys.argv[1], 'r').readlines()
-    new_file_lines = []
-    updated_line = None
-    with open(sys.argv[1], 'w') as file:
+    fileLines = open(sys.argv[1], 'r').readlines()
+    newFileLines = []
+    updatedLine = None
+    with open(sys.argv[1], 'w') as f:
         try:
-            for line in file_lines:
-                updated_line = RemoveParens(line)
-                new_file_lines.append(updated_line.line)
-            file.write(''.join(new_file_lines))
+            for line in fileLines:
+                updatedLine = RemoveParens(line)
+                newFileLines.append(updatedLine.line)
+            f.write(''.join(newFileLines))
         # If something goes wrong, write the file to it's original state
-        except:
-            file.write(''.join(file_lines))
+        except BaseException:
+            f.write(''.join(fileLines))
