c025e273ae2fe291875c44efbe68118dd46a66a7
Tyler Roberts <tjroberts314@gmail.com>
2017-11-16 15:12:53 -0500

    added more docstrings

--- rm_parens.py
+++ rm_parens.py
@@ -23,28 +23,39 @@ class RemoveParens(object):
         self._remove_parens()
 
     def _set_open_parens_index(self):
+        """
+        Set the index of the first open paren in the string, line.
+        """
         if 'if(' in self.line:
             index = self.line.index('if(') + 2
             self.line = self.line[:index] + ' ' + self.line[index:]
         elif 'while(' in self.line:
             index = self.line.index('while(') + 5
             self.line = self.line[:index] + ' ' + self.line[index:]
-        # Set index to be placed at the paren
+        # Set index to be placed at the opening paren
         match = re.match('[ \t]*(if|while)[ \t]*\(', self.line)
         open_paren_match = match[0] if six.PY3 else match.group(0)
         self._open_paren_index = self.line.index(
             open_paren_match) + len(open_paren_match) - 1
 
     def _set_close_parens_index(self):
+        """
+        Set the index of the last close paren in the string, 'line'.
+        """
         for char_i in range(len(self.line) - 1, -1, -1):
             if self.line[char_i] == ')':
                 self._close_paren_index = char_i
                 break
 
     def _unnecessary_parens(self):
-        if self._outside_parens():
+        """
+        Returns True if 'line' is an if or while statement, that has unnecessary
+        parentheses aorund its conditional. Returns False otherwise.
+        """
+        if self._has_outside_parens():
             self._set_open_parens_index()
             self._set_close_parens_index()
+            # Ensure that nested parens are not dependent on the outer parens 
             cnt_matching_parens = 0
             for i in range(self._open_paren_index + 1, self._close_paren_index):
                 if self.line[i] == '(':
@@ -57,12 +68,19 @@ class RemoveParens(object):
                 return True
         return False
 
-    def _outside_parens(self):
+    def _has_outside_parens(self):
+        """
+        Returns true if 'line' is an if or while statement, that has parentheses on the
+        outside of its conditional.
+        """
         if re.match('[ \t]*(if|while)[ \t]*\(.*\)[ \t]*:', self.line):
             return True
         return False
 
     def _remove_parens(self):
+        """
+        Removes unnecessary parentheses around an if or while statement's conditional.
+        """
         if self._unnecessary_parens():
             self.line = self.line[:self._open_paren_index] + self.line[self._open_paren_index + \
                 1:self._close_paren_index] + self.line[self._close_paren_index + 1:]
