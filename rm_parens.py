9ccc1d89fd35dd8e29bf3a8f64e8712514a99441
Tyler Roberts <tjroberts314@gmail.com>
2017-11-17 11:09:33 -0500

    initial commit, refactor as nested functions

--- rm_parens.py
+++ rm_parens.py
@@ -10,57 +10,59 @@ import re
 import six
 
 
-class RemoveParens(object):
+def removeParens(line):
     """
     Takes a string in and removes the unnecessary parenthesis, set the line class variable
     to the updated string. The line class variable will remain unchanged if no unnecessary
     parenthesis exist.
     """
-    def __init__(self, line):
-        self.line = line
-        self._open_paren_index = None
-        self._close_paren_index = None
-        self._remove_parens()
 
-    def _set_open_parens_index(self):
+    def get_open_parens_index(line):
         """
-        Set the index of the first open paren in the string, line.
+        Get the index of the first open paren in the string, line.
         """
-        if 'if(' in self.line:
-            index = self.line.index('if(') + 2
-            self.line = self.line[:index] + ' ' + self.line[index:]
-        elif 'while(' in self.line:
-            index = self.line.index('while(') + 5
-            self.line = self.line[:index] + ' ' + self.line[index:]
+        if 'if(' in line:
+            index = line.index('if(') + 2
+            line = line[:index] + ' ' + line[index:]
+        elif 'while(' in line:
+            index = line.index('while(') + 5
+            line = line[:index] + ' ' + line[index:]
         # Set index to be placed at the opening paren
-        match = re.match('[ \t]*(if|while)[ \t]*\(', self.line)
+        match = re.match('[ \t]*(if|while)[ \t]*\(', line)
         open_paren_match = match[0] if six.PY3 else match.group(0)
-        self._open_paren_index = self.line.index(
-            open_paren_match) + len(open_paren_match) - 1
+        return line.index(open_paren_match) + len(open_paren_match) - 1
 
-    def _set_close_parens_index(self):
+    def get_close_parens_index(line):
         """
-        Set the index of the last close paren in the string, 'line'.
+        Get the index of the last close paren in the string, 'line'.
         """
-        for char_i in range(len(self.line) - 1, -1, -1):
-            if self.line[char_i] == ')':
-                self._close_paren_index = char_i
-                break
+        for char_i in range(len(line) - 1, -1, -1):
+            if line[char_i] == ')':
+                return char_i
 
-    def _unnecessary_parens(self):
+    def has_outside_parens(line):
+        """
+        Returns true if 'line' is an if or while statement, that has parentheses on the
+        outside of its conditional.
+        """
+        if re.match('[ \t]*(if|while)[ \t]*\(.*\)[ \t]*:', line):
+            return True
+        return False
+
+    def unnecessary_parens(line):
         """
         Returns True if 'line' is an if or while statement, that has unnecessary
         parentheses aorund its conditional. Returns False otherwise.
         """
-        if self._has_outside_parens():
-            self._set_open_parens_index()
-            self._set_close_parens_index()
+        if has_outside_parens(line):
+            open_parens_index = get_open_parens_index(line)
+            close_parens_index = get_close_parens_index(line)
             # Ensure that nested parens are not dependent on the outer parens
             cnt_matching_parens = 0
-            for i in range(self._open_paren_index + 1, self._close_paren_index):
-                if self.line[i] == '(':
+            for i in range(open_parens_index + 1, close_parens_index):
+                if line[i] == '(':
                     cnt_matching_parens += 1
-                elif self.line[i] == ')':
+                elif line[i] == ')':
                     cnt_matching_parens -= 1
                     if cnt_matching_parens < 0:
                         return False
@@ -68,22 +70,10 @@ class RemoveParens(object):
                 return True
         return False
 
-    def _has_outside_parens(self):
-        """
-        Returns true if 'line' is an if or while statement, that has parentheses on the
-        outside of its conditional.
-        """
-        if re.match('[ \t]*(if|while)[ \t]*\(.*\)[ \t]*:', self.line):
-            return True
-        return False
-
-    def _remove_parens(self):
-        """
-        Removes unnecessary parentheses around an if or while statement's conditional.
-        """
-        if self._unnecessary_parens():
-            self.line = self.line[:self._open_paren_index] + self.line[self._open_paren_index + \
-                1:self._close_paren_index] + self.line[self._close_paren_index + 1:]
+    if unnecessary_parens(line):
+        return line[:get_open_parens_index(line)] + line[get_open_parens_index(line) + 1:get_close_parens_index(line)] + line[get_close_parens_index(line) + 1:]
+    else:
+        return line
 
 
 if __name__ == '__main__':
@@ -97,8 +87,7 @@ if __name__ == '__main__':
         with open(sys.argv[i], 'w') as f:
             try:
                 for line in fileLines:
-                    updatedLine = RemoveParens(line)
-                    newFileLines.append(updatedLine.line)
+                    newFileLines.append(removeParens(line))
                 f.write(''.join(newFileLines))
             # If something goes wrong, write the file to it's original state
             except BaseException:
