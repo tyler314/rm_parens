9a41d67c6016c56d00a79a4cca2b3dda243738db
Tyler Roberts <tjroberts314@gmail.com>
2017-11-18 15:01:16 -0500

    added more unittests

--- rm_parens.py
+++ rm_parens.py
@@ -7,8 +7,49 @@ code and the proper syntax.
 from __future__ import print_function
 import sys
 import re
-import six
 
+def _get_open_parens_index(string):
+    """
+    Return the index of the first open paren in the string.
+    """
+    if 'if(' in string:
+        index = string.index('if(') + 2
+        string = string[:index] + ' ' + string[index:]
+    elif 'while(' in string:
+        index = string.index('while(') + 5
+        string = string[:index] + ' ' + string[index:]
+    # Set index to be placed at the opening paren
+    match = re.match(r'[ \t]*(if|while)[ \t]*\(', string)
+    open_paren_match = match.group(0)
+    return string.index(open_paren_match) + len(open_paren_match) - 1
+
+def _get_close_parens_index(string):
+    """
+    Return the index of the last close paren in the string.
+    """
+    return len(string) - string[::-1].index(')') - 1
+
+def _unnecessary_parens(string):
+    """
+    Returns True if 'string' is an if or while statement, that has unnecessary
+    parentheses aorund its conditional. Returns False otherwise.
+    """
+    # Check if string has outside parentheses
+    if re.match(r'[ \t]*(if|while)[ \t]*\(.*\)[ \t]*:', string):
+        open_parens_index = _get_open_parens_index(string)
+        close_parens_index = _get_close_parens_index(string)
+        # Ensure that nested parens are not dependent on the outer parens
+        cnt_matching_parens = 0
+        for i in range(open_parens_index + 1, close_parens_index):
+            if string[i] == '(':
+                cnt_matching_parens += 1
+            elif string[i] == ')':
+                cnt_matching_parens -= 1
+                if cnt_matching_parens < 0:
+                    return False
+        if cnt_matching_parens == 0:
+            return True
+    return False
 
 def remove_parens(string):
     """
@@ -16,52 +57,9 @@ def remove_parens(string):
     the same input if no unnecessary parenthesis exist.
     """
 
-    def get_open_parens_index(string):
-        """
-        Return the index of the first open paren in the string.
-        """
-        if 'if(' in string:
-            index = string.index('if(') + 2
-            string = string[:index] + ' ' + string[index:]
-        elif 'while(' in string:
-            index = string.index('while(') + 5
-            string = string[:index] + ' ' + string[index:]
-        # Set index to be placed at the opening paren
-        match = re.match(r'[ \t]*(if|while)[ \t]*\(', string)
-        open_paren_match = match[0] if six.PY3 else match.group(0)
-        return string.index(open_paren_match) + len(open_paren_match) - 1
-
-    def get_close_parens_index(string):
-        """
-        Return the index of the last close paren in the string.
-        """
-        return len(string) - string[::-1].index(')') - 1
-
-    def unnecessary_parens(string):
-        """
-        Returns True if 'string' is an if or while statement, that has unnecessary
-        parentheses aorund its conditional. Returns False otherwise.
-        """
-        # Check if string has outside parentheses
-        if re.match(r'[ \t]*(if|while)[ \t]*\(.*\)[ \t]*:', string):
-            open_parens_index = get_open_parens_index(string)
-            close_parens_index = get_close_parens_index(string)
-            # Ensure that nested parens are not dependent on the outer parens
-            cnt_matching_parens = 0
-            for i in range(open_parens_index + 1, close_parens_index):
-                if string[i] == '(':
-                    cnt_matching_parens += 1
-                elif string[i] == ')':
-                    cnt_matching_parens -= 1
-                    if cnt_matching_parens < 0:
-                        return False
-            if cnt_matching_parens == 0:
-                return True
-        return False
-
-    if unnecessary_parens(string):
-        return string[:get_open_parens_index(string)] + string[get_open_parens_index(
-            string) + 1:get_close_parens_index(string)] + string[get_close_parens_index(string)\
+    if _unnecessary_parens(string):
+        return string[:_get_open_parens_index(string)] + string[_get_open_parens_index(
+            string) + 1:_get_close_parens_index(string)] + string[_get_close_parens_index(string)\
              + 1:]
     return string
 
--- tests/test_rm_parens.py
+++ tests/test_rm_parens.py
@@ -1,8 +1,137 @@
 import unittest
 import rm_parens
 
+
 class TestRmParens(unittest.TestCase):
-    
+
+    def test_get_open_parens_index(self):
+        """
+        _get_open_parens_index is a private function that is only to be used for
+        a string has an 'if' or 'while' followed by an open parens, thus error
+        checking for a strings without this assumption is not implemented.
+        """
+        for check in ['if', 'while']:
+            res = rm_parens._get_open_parens_index(check + ' (')
+            self.assertEqual(res, len(check) + 1)
+            # get_open_parens_index inserts a space between 'if' or 'while', and
+            # '(' if a space does not exist
+            res = rm_parens._get_open_parens_index(check + '(')
+            self.assertEqual(res, len(check) + 1)
+
+            res = rm_parens._get_open_parens_index(check + ' (condition)')
+            self.assertEqual(res, len(check) + 1)
+
+            res = rm_parens._get_open_parens_index(5 * ' ' + check + ' (')
+            self.assertEqual(res, len(check) + 6)
+
+            res = rm_parens._get_open_parens_index(5 * ' ' + check + '(')
+            self.assertEqual(res, len(check) + 6)
+
+            res = rm_parens._get_open_parens_index(
+                '{}(condition_{}(_unique)'.format(check, check))
+            self.assertEqual(res, len(check) + 1)
+
+            res = rm_parens._get_open_parens_index(
+                check + '(((some nest) and (another)))')
+            self.assertEqual(res, len(check) + 1)
+
+    def test_get_close_parens_index(self):
+        """
+        This private function assumes only that a closing parens exists
+        """
+        res = rm_parens._get_close_parens_index(')')
+        self.assertEqual(res, 0)
+        res = rm_parens._get_close_parens_index('()')
+        self.assertEqual(res, 1)
+        string = 'if (condition)'
+        res = rm_parens._get_close_parens_index(string)
+        self.assertEqual(res, len(string) - 1)
+        string = 'while(condition)'
+        res = rm_parens._get_close_parens_index(string)
+        self.assertEqual(res, len('while(condition)') - 1)
+        string = '(()())()))(()()(('
+        res = rm_parens._get_close_parens_index(string)
+        self.assertEqual(res, len(string) - 3)
+
+    def test_unnecessary_parens(self):
+        """
+        This private function returns True if the string is an 'if' or 'while'
+        statement, followed by a condition surrounded by parens that do not
+        contribute to the correctness of the conditional.
+        """
+        res = rm_parens._unnecessary_parens('if (condition):')
+        self.assertEqual(res, True)
+        res = rm_parens._unnecessary_parens('if condition:')
+        self.assertEqual(res, False)
+        res = rm_parens._unnecessary_parens('if (condition:')
+        self.assertEqual(res, False)
+        res = rm_parens._unnecessary_parens('if ((condition)):')
+        self.assertEqual(res, True)
+        res = rm_parens._unnecessary_parens('if ((condition) and (another)):')
+        self.assertEqual(res, True)
+        res = rm_parens._unnecessary_parens('if (condition) and (another):')
+        self.assertEqual(res, False)
+        res = rm_parens._unnecessary_parens(
+            'some comment ->>> (condition without if or while)')
+        self.assertEqual(res, False)
+        res = rm_parens._unnecessary_parens('(condtion)')
+        self.assertEqual(res, False)
+
     def test_remove_parens(self):
-        res = rm_parens('')
-        self.assertEqual(res, '')
+        """
+        Test the only public function, removes unnecessary parens around if and
+        while conditionals, returns result.
+        """
+        string = 'if (condition):'
+        res = rm_parens.remove_parens(string)
+        self.assertEqual(res, 'if condition:')
+
+        string = 'if ((condition)):'
+        res = rm_parens.remove_parens(string)
+        self.assertEqual(res, 'if (condition):')
+
+        string = 'if (condition) and (another):'
+        res = rm_parens.remove_parens(string)
+        self.assertEqual(res, string)
+
+        string = '(hello, world) just some text ())()('
+        res = rm_parens.remove_parens(string)
+        self.assertEqual(res, string)
+
+        string = 'if (condition) or ((another) and (nest)):'
+        res = rm_parens.remove_parens(string)
+        self.assertEqual(res, string)
+
+        string = 'if ((condition) or ((another) and (nest))):'
+        res = rm_parens.remove_parens(string)
+        self.assertEqual(res, 'if (condition) or ((another) and (nest)):')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
